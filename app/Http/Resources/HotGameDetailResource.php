3fbb8b09a9684f6797cfb5e0d28656c64151bde6
wlktech <stephensuu03@gmail.com>
2025-05-20 14:12:26 +0700

    a

--- app/Http/Resources/HotGameDetailResource.php
+++ app/Http/Resources/HotGameDetailResource.php
@@ -14,21 +14,16 @@ class HotGameDetailResource extends JsonResource
      */
     public function toArray(Request $request): array
     {
-        $imgUrl = $this->gameTypes[0]->pivot->image;
+        // $imgUrl = $this->gameTypes[0]->pivot->image;
 
         return [
             'id' => $this->id,
-            'provider_name' => $this->provider_name,
-            'imgUrl' => asset('assets/img/provider_logo/'.$imgUrl),
-            'hot_lists' => $this->gameLists->map(function ($game) {
-                return [
-                    'code' => $game->game_code,
-                    'name' => $game->game_name,
-                    'game_type_id' => $game->game_type_id,
-                    'provider_id' => $game->product_id,
-                    'image' => $game->image_url,
-                ];
-            }),
+            'code' => $this->code,
+            'name' => $this->name,
+            'game_type_id' => $this->game_type_id,
+            'provider_id' => $this->product_id,
+            'provider_code' => $this->product->code,
+            'image' => $this->image_url,
         ];
     }
 }
